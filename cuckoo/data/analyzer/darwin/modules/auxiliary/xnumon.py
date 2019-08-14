fb02a4940e6923be0d38ec25b786986c0ad799d8
TheSixthPhantom <chirag301212@gmail.com>
2019-08-14 13:51:29 +0530

    fix:CHECK_RELEVANCE

--- cuckoo/data/analyzer/darwin/modules/auxiliary/xnumon.py
+++ cuckoo/data/analyzer/darwin/modules/auxiliary/xnumon.py
@@ -3,17 +3,15 @@ from lib.common.process import TRACKED_PROCESSES
 import subprocess
 import socket
 import json
-import os
 import signal
 import sys
+import os
 
 class InitiateMonitor(object):
     def __init__(self, config):
         self.PID_PATH = '/private/var/run/xnumon.pid'
         self.RELEVANT_EVENTCODES = (2,3,4)
         self.XNUMON_PATH = '/usr/local/sbin'
-        #change working directory
-        os.chdir(self.XNUMON_PATH)
         #store configurations
         self.config = config
 
@@ -38,7 +36,7 @@ class InitiateMonitor(object):
 
     def _check_relevance(self,log):
         json_string = json.loads(log)
-        if json_string["eventcodes"] in self.RELEVANT_EVENTCODES:
+        if json_string["eventcode"] in self.RELEVANT_EVENTCODES:
             if json_string["subject"]["pid"] in TRACKED_PROCESSES or json_string["subject"]["image"]["exec_pid"] in TRACKED_PROCESSES:
                 return True
             elif json_string["subject"]["ancestors"]:
@@ -46,29 +44,36 @@ class InitiateMonitor(object):
                     if ancestor["exec_pid"] in TRACKED_PROCESSES:
                         TRACKED_PROCESSES.append(json_string["subject"]["pid"])
                         return True
+                    else:
+                        return False
+            else:
+                return False
         else:
             return False
 
 
     def _log(self):
-        buffer_events = []
+        #buffer_events = []
         socket_host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_host.connect((self.config.ip, self.config.port))
         socket_host.send("JSON\n")
-        for log in self._execute(["sudo", "./xnumon", "-d"]):
+        for log in self._execute(["sudo", "/usr/local/sbin/xnumon", "-d"]):
             if TRACKED_PROCESSES:
-                iteration_control = True
-                #if there are buffer logged before sample is triggered
-                if buffer_events:
-                    while iteration_control:
-                        for item in buffer_events:
-                            if self._check_relevance(item):
-                                socket_host.send(item.encode())
-                        iteration_control = False
-                #check if log is relevant to target anyways
                 if self._check_relevance(log):
                     socket_host.send(log.encode())
-            else:
-                socket_host.send(log.encode())
+
+            #     iteration_control = True
+            #     #if there are buffer logged before sample is triggered
+            #     if buffer_events:
+            #         while iteration_control:
+            #             for item in buffer_events:
+            #                 if self._check_relevance(item):
+            #                     socket_host.send(item.encode())
+            #             iteration_control = False
+            #     #check if log is relevant to target anyways
+            #     if self._check_relevance(log):
+            #         socket_host.send(log.encode())
+            # else:
+            #     buffer_events += log.encode()
 
 
